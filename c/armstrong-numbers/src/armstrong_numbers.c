8142197ffb512440bc70fe2b5a806e828e88ab03
JustinSHong <jhong2009@gmail.com>
2020-01-26 20:01:01 -0800

    armstrong_numbers: add first solution

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "armstrong_numbers.h"
+
+int is_armstrong_number(int candidate)
+{
+    int len = snprintf(NULL, 0, "%d", candidate);
+    int current, remainder, temp = 1, sum = 0;
+
+    current = candidate;
+
+    while (current >= 1)
+    {
+        remainder = current % 10;
+        for (int i = len; i >= 1; i--)
+        {
+            temp *= remainder;
+        }
+        sum += temp;
+        current /= 10;
+        temp = 1;
+    }
+
+    if (sum == candidate)
+    {
+        return 1;
+    }
+    return 0;
+}
