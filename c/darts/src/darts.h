e01f932c2b2dd9e2bedfc84a478800d378513d76
JustinSHong <jhong2009@gmail.com>
2020-01-30 21:23:09 -0800

    fix typo

--- c/darts/src/darts.h
+++ c/darts/src/darts.h
@@ -2,7 +2,7 @@
 #ifndef DARTS_H
 #define DARTS_H
 
-typedef uint8_t coordinate_T[2 * sizeof(uint8_t)];
-int score(coordinate_T);
+typedef uint8_t coordinate_t[2 * sizeof(uint8_t)];
+int score(coordinate_t);
 
 #endif
