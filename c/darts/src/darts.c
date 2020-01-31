b1d4e268acbb4700de11c53fcf68480998c90f65
JustinSHong <jhong2009@gmail.com>
2020-01-30 23:31:05 -0800

    remove print statements

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -19,25 +19,18 @@ enum scores
 int score(coordinate_t coord)
 {
 
-    printf("COORD 1 %f\n", coord.x);
-    printf("COORD 2 %f\n", coord.y);
-
     float distance = sqrtf(powf(coord.x, 2) + powf(coord.y, 2));
-    printf("DISTANCE IS %f\n", distance);
 
     if (distance > 10)
     {
-        printf("DART IS OUTSIDE THE BOARD\n");
         return OUTSIDE;
     }
     else if (distance <= 10 && distance > 5)
     {
-        printf("DART IS IN OUTER CIRCLE\n");
         return OUTER;
     }
     else if (distance <= 5 && distance > 1)
     {
-        printf("DART IS IN MIDDLE CIRCLE\n");
         return MIDDLE;
     }
     else
